60030b47e447b18f191cca17718967a553e8425c
JostonChan <44818157+JostonChan@users.noreply.github.com>
2019-11-12 21:24:48 -0500

    Update Jostonsql.sql

--- JostonCIS/private_html/sql/Jostonsql.sql
+++ JostonCIS/private_html/sql/Jostonsql.sql
@@ -1,14 +1,13 @@
-select * from album;
-select * from user;
-select * from song;
-select * from artist;
 select * from playlist;
-select * from user_playlist;
-select * from media_type;
+select * from song_in_playlist;
+select * from song;
+
+-- INSERT INTO song_in_playlist  values
+--     (DEFAULT, 3, 9, DEFAULT);
 
 select Album_Name, Artist_Name from album
 	join user_album on Album_ID = Album_FK
-	join User on User_ID = User_FK
-	join user_artist on User_ID = User_FK
-	join artist on Artist_ID = Artist_FK 
+	join User on User.User_ID = User_album.User_FK
+	join user_artist on User.User_ID = User_artist.User_FK
+	join artist on Artist_ID = Artist_FK;
 
