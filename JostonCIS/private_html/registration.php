556252a259d5948d26c34b297fc8aeed338d51f5
ns1326 <56000743+ns1326@users.noreply.github.com>
2019-11-15 15:22:38 -0500

    Add files via upload

--- JostonCIS/private_html/registration.php
+++ JostonCIS/private_html/registration.php
@@ -1,7 +1,7 @@
 <?php
-    require_once "config.inc.php";
-    require_once "dbconfig.inc.php";
-    
+session_start();
+?>
+<?php
 // define variables and initialize with empty values
 $firstname = $lastname = $username = $passwordStored = $password = $country = $dob = "";
 
@@ -74,7 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <script>
     let timer = setTimeout(function () {
-        window.location = 'login.html'
+        window.location = 'profile.html'
     }, 5000);
 </script>
 
