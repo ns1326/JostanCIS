5d4f32bbe88809cd2db76af39b2e562c84f361d6
ns1326 <56000743+ns1326@users.noreply.github.com>
2019-12-11 20:36:19 -0500

    Add files via upload

--- JostonCIS/public_html/album/viewalbum.php
+++ JostonCIS/public_html/album/viewalbum.php
@@ -1,5 +1,36 @@
 <?php
-    require_once "../../private_html/config.inc.php";
-    require_once PRIVATE_HTML . "dbconfig.inc.php";
+require_once "../private_html/config.inc.php";
+require_once PRIVATE_HTML . "dbconfig.inc.php";
 
-$smarty->display("album/viewalbum.tpl");
+$sql = "SELECT Song_ID as SongID, Song.Title as Title , Album_Name, Artist_Name, Playlist_Name FROM Song	           
+            JOIN Album ON Album.Album_ID = Song.Album_FK
+            JOIN Artist ON Artist.Artist_ID = Song.Artist_FK    
+            JOIN Song_In_Playlist ON Song.Song_ID = Song_In_Playlist.Song_FK 
+            JOIN Playlist ON Playlist.Playlist_ID = Song_In_Playlist.Playlist_FK
+            JOIN User_Playlist ON Playlist.Playlist_ID = User_Playlist.Playlist_FK	
+        Order BY Song_ID";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+$nosongs = false;
+if($stmt->rowCount() == 0){
+    $nosongs = true;
+} else {
+    $songs = array();
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        $songs[] = array(
+            "SongID" => $row['SongID'],
+            "Title" => $row['Title'],
+            "Album_Name" => $row['Album_Name'],
+            "Artist_Name" => $row['Artist_Name'],
+            "Playlist_Name" => $row['Playlist_Name']
+        );
+    }
+}
+
+$song = ($songs[$_GET["id"]]);
+
+
+$smarty->assign("nosongs", $nosongs);
+$smarty->assign("songs", $songs);
+$smarty->display("viewalbum.tpl");
