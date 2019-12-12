9128d97644037bf2e7d9e3a30622e42f75c43b53
JostonChan <44818157+JostonChan@users.noreply.github.com>
2019-12-12 02:05:00 -0500

    redefine the error checking for all files

--- JostonCIS/public_html/changepassword.php
+++ JostonCIS/public_html/changepassword.php
@@ -7,6 +7,8 @@ $message2 = "";
 
 if (isset($_POST['submit'])) {
 
+    $count = 2;
+
     $sql = "Select * FROM User Where Salt = :salt";
     $stmt = $pdo->prepare($sql);        
     $stmt->bindParam(":salt", $_POST['salt']);     
@@ -19,37 +21,41 @@ if (isset($_POST['submit'])) {
         }
     } 
 
-    if ($_POST['salt'] == $salt && $_POST['salt'] != "") {
-        if ($_POST['password'] != "" || $_POST['password'] != null) {              
-        
-            if ( $_POST['password'] ==  $_POST['confirmpassword']) {
-                $sql = "Update User 
-                            set Password = :p 
-                        Where User_ID = :u";
+    if ($_POST['salt'] != $salt || $_POST['salt'] == "" || $_POST['salt'] == null ) {
+        $message1 = "Temporary Password does not exist.";
+        $count -= 1;
+    } 
 
-                $stmt = $pdo->prepare($sql);        
-                $stmt->bindParam(":p", $_POST['password']);  
-                $stmt->bindParam(":u", $userid);  
-                $stmt->execute();
-
-                $sql = "Update User 
-                            set SALT = DEFAULT
-                        Where User_ID = :u";
-
-                $stmt = $pdo->prepare($sql);        
-                $stmt->bindParam(":u", $userid);  
-                $stmt->execute();
-
-    
-                header("location: login.php");
-    
-            } 
-            $message2 = "Password is not the same";
-        } else {
-            $message1 = "Password and confirm cannot be blank.";
-        }            
+    if ($_POST['password'] != "" || $_POST['password'] != null) {      
+        if ( $_POST['password'] !=  $_POST['confirmpassword']) {
+            $message2 = "Password is not the same.";
+            $count -= 1;
+        } 
     } else {
-        $message1 = "Temporary Password is incorrect";
+        $message2 = "Password and confirm cannot be blank.";
+        $count -= 1;
+    }     
+
+    if ($count == 2) {
+        $sql = "Update User 
+                        set Password = :p 
+                    Where User_ID = :u";
+
+            $stmt = $pdo->prepare($sql);        
+            $stmt->bindParam(":p", $_POST['password']);  
+            $stmt->bindParam(":u", $userid);  
+            $stmt->execute();
+
+            $sql = "Update User 
+                        set SALT = DEFAULT
+                    Where User_ID = :u";
+
+            $stmt = $pdo->prepare($sql);        
+            $stmt->bindParam(":u", $userid);  
+            $stmt->execute();
+
+
+            header("location: login.php");
     }
 }
 
